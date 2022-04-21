cc6ec5748f6a2e5d8f81270c294d9e2a559b2ebe
Ljubomir Banovic <ljuba.banovic@gmail.com>
2022-04-22 00:29:38 +0200

    add changing size of canvas functionality

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
 let sizeOfCanvas = 16;
 
+let pixels = null;
 const canvas = document.querySelector("#canvas");
 
-for(let i = 0; i < sizeOfCanvas * sizeOfCanvas; ++i) {
-    const pixel = document.createElement("div");
-    pixel.classList.add('pixel');
-    canvas.appendChild(pixel);
+function createCanvas() {
+    while(canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild);
+    }
+
+    if(pixels) {
+        Array.from(pixels).forEach((pixel) => {
+            pixel.removeEventListener('mousedown', (e) => {
+                console.log("click");
+                let newSizeOfCanvas = +document.querySelector("#sizeOfCanvas").value;
+                pixel.style['background-color'] = document.querySelector("#inputColorPicker").value;
+            });
+        });
+    }
+
+    for(let i = 0; i < sizeOfCanvas * sizeOfCanvas; ++i) {
+        const pixel = document.createElement("div");
+        pixel.classList.add('pixel');
+        canvas.appendChild(pixel);
+    }
+
+    const canvasStyles = window.getComputedStyle(canvas);
+    const canvasWidth = canvas.clientWidth - (parseFloat(canvasStyles.paddingTop) + parseFloat(canvasStyles.paddingBottom));
+    const canvasHeight = canvas.clientHeight - (parseFloat(canvasStyles.paddingLeft) + parseFloat(canvasStyles.paddingRight));
+
+    pixels = document.querySelectorAll('.pixel');
+    console.dir(pixels);
+
+    Array.from(pixels).forEach((pixel) => {
+        pixel.style.width = `${canvasWidth / sizeOfCanvas}px`;
+        pixel.style.height = `${canvasHeight / sizeOfCanvas}px`;
+    });
+
+    Array.from(pixels).forEach((pixel) => {
+        pixel.addEventListener('mousedown', (e) => {
+            console.log("click");
+            let newSizeOfCanvas = +document.querySelector("#sizeOfCanvas").value;
+            pixel.style['background-color'] = document.querySelector("#inputColorPicker").value;
+        });
+    });
 }
 
-const canvasStyles = window.getComputedStyle(canvas);
-const canvasWidth = canvas.clientWidth - (parseFloat(canvasStyles.paddingTop) + parseFloat(canvasStyles.paddingBottom));
-const canvasHeight = canvas.clientHeight - (parseFloat(canvasStyles.paddingLeft) + parseFloat(canvasStyles.paddingRight));
+createCanvas();
 
-const pixels = document.querySelectorAll('.pixel');
+let canvasSizeSlider = document.querySelector("#sizeOfCanvas");
 
-Array.from(pixels).forEach((pixel) => {
-    // pixel.style.width = `${+canvas.style['width'] / sizeOfCanvas}px`;
-    // pixel.style.height = `${+canvas.style['height'] / sizeOfCanvas}px`;
-    pixel.style.width = `${canvasWidth / sizeOfCanvas}px`;
-    pixel.style.height = `${canvasHeight / sizeOfCanvas}px`;
-});
-
-const currentColor = 'red';
-
-Array.from(pixels).forEach((pixel) => {
-    pixel.addEventListener('mousedown', (e) => {
-        pixel.style['background-color'] = document.querySelector("#inputColorPicker").value;
-    });
+canvasSizeSlider.addEventListener('change', () => {
+    if(canvasSizeSlider.value !== sizeOfCanvas) {
+        sizeOfCanvas = +canvasSizeSlider.value;
+        createCanvas();
+    }
 });
 
 function clearCanvas() {
